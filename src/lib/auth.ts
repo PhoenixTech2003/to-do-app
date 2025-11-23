2a20df5c96a711ac8aefd8e50e3b67895e9c4242
Chiyembekezo Chilembwe <phoenixtech2003@gmail.com>
2025-11-23 17:32:09 +0200

    Setup the auth schema for drizzle

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,11 @@
 import { betterAuth } from "better-auth";
 import { db } from "./server/db";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import * as authSchema from "./server/db/schema/auth-schema"
 
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
-        provider: "pg", // or "mysql", "sqlite"
+        provider: "pg",
+        schema: authSchema
     }),
 });
