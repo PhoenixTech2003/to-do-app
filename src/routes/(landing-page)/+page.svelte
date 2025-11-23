c5ee7cb9f9ce0371f025b5e0611da5218321c163
Chiyembekezo Chilembwe <phoenixtech2003@gmail.com>
2025-11-23 10:07:33 +0200

    Setup the signin and signup pages

--- src/routes/(landing-page)/+page.svelte
+++ src/routes/(landing-page)/+page.svelte
@@ -1,4 +1,4 @@
-<h1 class="text-3xl">Welcome to the tichite to do application</h1>
+<h1 class="text-3xl">Welcome to the TwoDo to do application</h1>
 <p>Your all in one to do list application</p>
 <a href="/signup">
 	<button> Sign Up </button>
