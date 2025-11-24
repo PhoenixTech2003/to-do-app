3ca9914bcd65ee4b5133648dd138daf5eb55bb9b
Chiyembekezo Chilembwe <phoenixtech2003@gmail.com>
2025-11-24 04:09:43 +0200

    Finish adding auth flow

--- src/routes/(auth)/signin/+page.svelte
+++ src/routes/(auth)/signin/+page.svelte
@@ -1,1 +1,19 @@
-<p>Hello from the signin page</p>
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button/index';
+	import { authClient } from '$lib/auth';
+	import { toast } from 'svelte-sonner';
+
+	function handleGoogleSignIn() {
+		const googleSignInHandler = authClient.signIn.social({
+			provider: 'google',
+			callbackURL: '/dashboard'
+		});
+		toast.promise(googleSignInHandler, {
+			loading: 'Signing In',
+			error: 'An error occured while signing in with Google'
+		});
+	}
+</script>
+
+<p>Sign In to TwoDo</p>
+<Button onclick={handleGoogleSignIn}>Sign In with Google</Button>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
-	
+	import { Toaster } from '$lib/components/ui/sonner';
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
-
+<Toaster richColors />
 {@render children()}
